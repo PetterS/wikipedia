4d3645d3ae8c95f63043d3a690b16067c19c511c
Petter Strandmark <petter.strandmark@gmail.com>
2012-05-21 14:14:02 +0200

    Cache file

--- wanted_pages.py
+++ wanted_pages.py
@@ -2,6 +2,8 @@
 
 import sys
 import optparse
+import os.path
+import cPickle as pickle
 import xml.etree.cElementTree as ElementTree
 from bz2 import BZ2File
 import re
@@ -15,76 +17,96 @@ parser = optparse.OptionParser(usage)
 filename = 'svwiki-20120514-pages-meta-current.xml.bz2'
 if len(args) > 0 :
     filename = args[0]
+    
+datafilename = filename + '.cache'
 
-file = BZ2File(filename,'r')
 
-wikilink = re.compile(r'\[\[(.*?)\]\]')
+if not os.path.exists(datafilename) :
+    print 'Creating cache file...'
+    
+    file = BZ2File(filename,'r')
 
-all_pages = dict()
-number_of_links = dict()
+    wikilink = re.compile(r'\[\[(.*?)\]\]')
 
-iterations = 0
-title = ''
-text = ''
-ns = ''
-for event, elem in ElementTree.iterparse(file, events=('start', 'end')):
-    #Remove "{extra}" from "{extra}tag"
-    tag = elem.tag[ elem.tag.find('}') + 1: ]
-    #print event, tag
-    if event == 'start' and tag == 'page' :
-        title = ''
-        text = ''
-        ns = ''
-        links_on_page = dict()
-    elif event == 'end' and tag == 'title' :
-        title = elem.text
-        elem.clear()
-    elif event == 'end' and tag == 'text' :
-        text = elem.text
-        elem.clear()
-    elif event == 'end' and tag == 'ns' :
-        ns = elem.text
-        elem.clear()
-    elif event == 'end' and tag == 'page' :
-        
-        # Add to pages dictionary
-        all_pages[title] = 1
-        
-        # Only work in main name space
-        if ns == '0':
-            #Extract everything between [[ ]] tags
-            links = wikilink.findall(text)
-            for link in links :
-                # Does the link contain a '|'?
-                p = link.find('|') 
-                if p >= 0 : 
-                    link = link[:p]
+    all_pages = dict()
+    number_of_links = dict()
+
+    iterations = 0
+    title = ''
+    text = ''
+    ns = ''
+    for event, elem in ElementTree.iterparse(file, events=('start', 'end')):
+        #Remove "{extra}" from "{extra}tag"
+        tag = elem.tag[ elem.tag.find('}') + 1: ]
+        #print event, tag
+        if event == 'start' and tag == 'page' :
+            title = ''
+            text = ''
+            ns = ''
+            links_on_page = dict()
+        elif event == 'end' and tag == 'title' :
+            title = elem.text
+            elem.clear()
+        elif event == 'end' and tag == 'text' :
+            text = elem.text
+            elem.clear()
+        elif event == 'end' and tag == 'ns' :
+            ns = elem.text
+            elem.clear()
+        elif event == 'end' and tag == 'page' :
             
-                # First character upper case
-                if len(link) > 0 :
-                    link = upper(link[0]) + link[1:]
+            # Add to pages dictionary
+            all_pages[title] = 1
             
-                # If this link is not already on this page
-                if not links_on_page.has_key(link) :
-                    links_on_page[link] = 1
-                    # Does this link exist in the dictionary?
-                    if number_of_links.has_key(link) :
-                        number_of_links[link] += 1
-                    else :
-                        number_of_links[link] = 1
+            # Only work in main name space
+            if ns == '0':
+                #Extract everything between [[ ]] tags
+                links = wikilink.findall(text)
+                for link in links :
+                    # Does the link contain a '|'?
+                    p = link.find('|') 
+                    if p >= 0 : 
+                        link = link[:p]
+                
+                    # First character upper case
+                    if len(link) > 0 :
+                        link = upper(link[0]) + link[1:]
+                
+                    # If this link is not already on this page
+                    if not links_on_page.has_key(link) :
+                        links_on_page[link] = 1
+                        # Does this link exist in the dictionary?
+                        if number_of_links.has_key(link) :
+                            number_of_links[link] += 1
+                        else :
+                            number_of_links[link] = 1
+                
+            elem.clear()
             
-        elem.clear()
-        
-    # Print some progress every now and then
-    iterations+=1
-    if iterations % 10000 == 0 :
-        sys.stdout.write('\r')
-        sys.stdout.write('%d pages processed.  (%d XML events)            ' % (len(all_pages), iterations) )
-        
-print ''
-        
-sorted_links = sorted( number_of_links, key=number_of_links.get, reverse=True)
-
+        # Print some progress every now and then
+        iterations+=1
+        if iterations % 10000 == 0 :
+            sys.stdout.write('\r')
+            sys.stdout.write('%d pages processed.  (%d XML events)            ' % (len(all_pages), iterations) )
+            
+    print ''
+            
+    sorted_links = sorted( number_of_links, key=number_of_links.get, reverse=True)
+    
+    # Save to cache file
+    with open(datafilename, 'wb') as f:
+        pickle.dump(all_pages,f)
+        pickle.dump(number_of_links,f)
+        pickle.dump(sorted_links,f)
+    
+else :
+    print 'Using cache file...'
+    with open(datafilename, 'rb') as f:
+        all_pages = pickle.load(f)
+        number_of_links = pickle.load(f)
+        sorted_links = pickle.load(f)
+    
+    
 for i in range(100) :
     page = sorted_links[i]
     
