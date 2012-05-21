7ce2825b83183b9452d32d66ed049618222af10d
Petter Strandmark <petter.strandmark@gmail.com>
2012-05-21 17:46:35 +0200

    Ignore prefix :src:'

--- wanted_pages.py
+++ wanted_pages.py
@@ -31,7 +31,7 @@ outputfilename = filename + '.wiki'
 headerfilename = 'header.wiki'
 footerfilename = 'footer.wiki'
 
-ignored_prefixes = ['File:', 'Fil:', 'Bild:', 'Image:', 'Kategori:']
+ignored_prefixes = ['File:', 'Fil:', 'Bild:', 'Image:', 'Kategori:', ':src:']
 def is_page(page) :
     for prefix in ignored_prefixes : 
         if page.startswith(prefix) :
